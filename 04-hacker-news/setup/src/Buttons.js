42a336df0d488a16663e497ca29fda9d76eb532d
robertfodor-job <79786361+robertfodor-job@users.noreply.github.com>
2022-01-04 16:10:17 +0100

    hacker-news

---
+++ 04-hacker-news/setup/src/Buttons.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useGlobalContext } from './context';
+
+const Buttons = () => {
+  const { isLoading, page, nbPages, handlePage } = useGlobalContext();
+
+  return (
+    <div className="btn-container">
+      <button onClick={() => handlePage('dec')} disabled={isLoading}>
+        prev
+      </button>
+      <p>
+        {page + 1} of {nbPages}
+      </p>
+      <button onClick={() => handlePage('inc')} disabled={isLoading}>
+        next
+      </button>
+    </div>
+  );
+};
+
+export default Buttons;
